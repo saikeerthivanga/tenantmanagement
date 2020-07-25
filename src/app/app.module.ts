7a2ec157f80160cec2106a2be26256e1499a567e
Sathvika Macha <ragasiri29@gmail.com>
2020-07-26 01:59:15 +0530

    completed create-property component frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,16 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire'
 import { environment} from '../environments/environment.prod';
 import { LoginComponent } from './login/login.component';
-import { CreatePropertyComponent } from './create-property/create-property.component'
+import { CreatePropertyComponent } from './create-property/create-property.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -17,7 +26,17 @@ import { CreatePropertyComponent } from './create-property/create-property.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
